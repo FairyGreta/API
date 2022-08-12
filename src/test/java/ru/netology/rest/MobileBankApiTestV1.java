38e9f5fe9d57e269d52504f0a4e5e4b589464f67
FairyGreta <Christina_Art@mail.ru>
2022-08-12 13:39:36 +0500

    Add test JSONSchema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -8,6 +8,17 @@ import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
 
+    //проверим схему
+    @Test
+    void shouldReturnDemoAccountJSONScheme () {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
 
     @Test
     void shouldReturnDemoAccounts() {
